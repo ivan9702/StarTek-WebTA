6f6bec8346fe9fe6ece984fa42f1885d577b4da8
garfield <shaudai0402@hotmail.com>
2018-04-10 10:15:15 +0800

    Load Bootstrap's CSS
    1. npm install bootstrap --save
    2. Use express.static to register Bootstrap css file at the right URL
    3. Add the Bootstrap css to the template

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css/bootstrap.css', express.static('node_modules/bootstrap/dist/css/bootstrap.min.css'));
 
 app.use('/', index);
 app.use('/users', users);
