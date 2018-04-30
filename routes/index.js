8eb84a85f75dd767f9b3fbb566edc279bdd7b822
garfield <garfield@mail.startek-eng.com>
2018-04-30 14:53:52 +0800

    Add footer for displaying version number

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const pJson = require('../package.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'STARTEK WebTA' });
+  res.render('index', {
+    title: 'STARTEK WebTA',
+    version: pJson.version
+  });
 });
 
 module.exports = router;
