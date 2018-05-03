76c7b1b76b61157b2c62820124edf68eab0af0d5
garfield <garfield@mail.startek-eng.com>
2018-05-03 09:37:06 +0800

    Move post() function to a separate js file for common use

--- public/javascripts/page-script.js
+++ public/javascripts/page-script.js
@@ -89,26 +89,3 @@ function saveTaRecord(userId, dateTime) {
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify({userId, dateTime}));
 }
-
-function post(path, params, method) {
-  method = method || "post";
-
-  const form = document.createElement("form");
-  form.setAttribute("method", method);
-  form.setAttribute("action", path);
-  form.setAttribute("target", "_blank");
-
-  for(let key in params) {
-    if(params.hasOwnProperty(key)) {
-      var hiddenField = document.createElement("input");
-      hiddenField.setAttribute("type", "hidden");
-      hiddenField.setAttribute("name", key);
-      hiddenField.setAttribute("value", params[key]);
-
-      form.appendChild(hiddenField);
-    }
-  }
-
-  document.body.appendChild(form);
-  form.submit();
-}
