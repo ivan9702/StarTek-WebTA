2319fa7f0522bb817ece3535da314977851ead3c
garfield <garfield@mail.startek-eng.com>
2018-06-20 11:35:05 +0800

    Set idle threshold as 300 sec for enroll button click event, keep 120 sec for other events

--- public/javascripts/detectIdle.js
+++ public/javascripts/detectIdle.js
@@ -1,14 +1,18 @@
 var idleTime = 0;
+var threshold = 120;
+const enrollBtn = document.getElementById('enroll');
 
 document.addEventListener('keyup', () => {
   console.log('document keyup event reset');
   idleTime = 0;
 });
 window.addEventListener('message', () => {
+  threshold = 120;
   console.log('window message event reset');
   idleTime = 0;
 });
-document.addEventListener('click', () => {
+document.addEventListener('click', function(event) {
+  threshold = (event.target === enrollBtn) ? 300 : 120;
   console.log('document click event reset');
   idleTime = 0;
 });
@@ -17,7 +21,8 @@ function timerIncrement() {
   setTimeout(timerIncrement, 1000);
   idleTime = idleTime + 1;
   console.log('idleTime: ', idleTime);
-  if (idleTime > 120) {
+  console.log('threshold: ', threshold);
+  if (idleTime > threshold) {
     window.location.href = originURL;
   }
 }
