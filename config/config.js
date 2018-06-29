88f468a041cd3b60d99f2018dc5d4a6b3566a529
garfield <garfield@mail.startek-eng.com>
2018-06-29 10:27:12 +0800

    update config.js for database configuration

--- config/config.js
+++ config/config.js
@@ -1,23 +1,14 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL'
   }
-}
+};
