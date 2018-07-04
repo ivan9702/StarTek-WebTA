9ecb8dec558af9533920741b96001404fbbe34b6
garfield <garfield@mail.startek-eng.com>
2018-07-04 13:39:32 +0800

    use sequelize 'create()' method to add a user to webTA

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,36 @@
-const { User } = require('../models/user');
+const { User } = require('../models');
 const { arrAdminUser, allUserId, allUserName } = require('./admin.js');
 
-exports.createUser = (req, res, next) => {
+
+exports.createUser = async (req, res, next) => {
   const data = req.body;
-  let resMsg = 'User is created';
-  User.create(data, err => {
-    if (err) {
-      if (err.errno === 19) {
-        return res.status(200).send('User already exists');
-      } else {
-        return next(err);
-      }
-    }
-    if (data.PrivilegeId === '1') {
-      arrAdminUser.push(data.UserName);
-      if (arrAdminUser.length === 1) resMsg = 'First admin is created';
-    }
-    allUserId.push(data.UserName);
-    allUserName.push(data.NameString);
-    res.status(201).send({
-      resMsg,
-      adminUser: arrAdminUser,
-      allUserId,
-      allUserName
+  let resMsg = `User ${data.UserName} is created`;
+
+  try {
+    const user = await User.create(data, {
+      fields: ['UserName', 'NameString', 'PrivilegeId']
     });
+  }
+  catch (e) {
+    if (e.name === 'SequelizeUniqueConstraintError') {
+      resMsg = `User ${data.UserName} already exists`;
+      return res.status(200).send(resMsg);
+    } else {
+      return next(e);
+    }
+  }
+
+  if (data.PrivilegeId === '1') {
+    arrAdminUser.push(data.UserName);
+    if (arrAdminUser.length === 1) resMsg = 'First admin is created';
+  }
+
+  allUserId.push(data.UserName);
+  allUserName.push(data.NameString);
+  res.status(201).send({
+    resMsg,
+    adminUser: arrAdminUser,
+    allUserId,
+    allUserName
   });
 };
