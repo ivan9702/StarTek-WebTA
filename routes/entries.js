b1543e60c1283dfa742a4e4942de583953fbb934
garfield <garfield@mail.startek-eng.com>
2018-07-06 09:17:05 +0800

    complete 'filter' implementation for the query results

--- routes/entries.js
+++ routes/entries.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const { Entry, Location, Event, User } = require('../models');
 const { arrAdminUser } = require('./admin.js');
 const pJson = require('../package.json');
@@ -9,7 +11,7 @@ const userMap = new Map();
 const syncUserMap = async () => {
   try {
     const users = await User.findAll({
-      fields: ['UserId', 'UserName']
+      fields: ['UserId', 'UserName', 'NameString']
     });
     users.map(user => {
       userMap.set(user.get('UserName'), user.get('UserId'));
@@ -68,8 +70,9 @@ exports.listAll = async (req, res, next) => {
     });
 
     const entries = entriesModel.map(entry => {
+      const dbDateTime = entry.get('dateTime');
       return {
-        dateTime: entry.get('dateTime'),
+        dateTime: dbDateTime.length > 20 ? createTimeStr(dbDateTime) : dbDateTime,
         event: entry.Event.Name,
         id: entry.User.UserName,
         location: entry.Location.IpAddress,
@@ -109,7 +112,7 @@ exports.addEntry = async (req, res, next) => {
   }
 
   const entryToBeWritten = {
-    DateTime: data.dateTime + 'Z',
+    DateTime: data.dateTime,
     EventId: eventMap.get(data.event),
     LocationId: locationMap.get(data.ipAddress),
     UserId: userMap.get(data.userId)
@@ -127,7 +130,7 @@ exports.addEntry = async (req, res, next) => {
   }
 };
 
-exports.filter = (req, res, next) => {
+exports.filter = async (req, res, next) => {
   let { userId, listAll, dtStart, dtEnd, querierId } = req.body;
 
   const conditions = {
@@ -136,54 +139,98 @@ exports.filter = (req, res, next) => {
     dtEnd: (listAll === 'true') ? dtEnd : (dtEnd.slice(0, 10) + ' 23:59'),
   };
 
-  Entry.filter(conditions, (err, entries) => {
-    if (err) return next(err);
-
-    const renderInfo = {
-      title: 'Filtered Records',
-      userId,
-      querierId,
-      listAll,
-      dateTime: {
-        date: {
-          start: conditions.dtStart.slice(0, 10),
-          end: conditions.dtEnd.slice(0, 10),
-        },
-        time: {
-          start: conditions.dtStart.slice(11, 16),
-          end: conditions.dtEnd.slice(11, 16)
-        }
-      },
-      isAdmin: arrAdminUser.includes(querierId),
-      home: req.protocol + '://' + req.headers.host,
-      version: pJson.version
-    };
-
-    if (listAll === 'false') {
-      const taStarts = [];
-      const taEnds = [];
-      if (entries.length > 0) {
-        taStarts.push(entries[0]);
-        for (let i = 1; i < entries.length; i++) {
-          if (entries[i].dateTime.slice(0, 10) !== entries[i - 1].dateTime.slice(0, 10)) {
-            taEnds.push(entries[i - 1]);
-            taStarts.push(entries[i]);
-          }
-        }
-        taEnds.push(entries[entries.length - 1]);
+  const entriesModel = await Entry.findAll({
+    attributes: [
+      ['DateTime', 'dateTime']
+    ],
+    include: [{
+      model: Location,
+      required: true
+    }, {
+      model: Event,
+      required: true
+    }, {
+      model: User,
+      required: true,
+      attributes: {
+        exclude: ['DepartmentId', 'PrivilegeId']
       }
-      res.render('entries', {
-        ...renderInfo,
-        taStarts,
-        taEnds
-      });
-    } else {
-      res.render('entries', {
-        ...renderInfo,
-        entries
-      });
+    }],
+    where: {
+      UserId: userMap.get(userId),
+      DateTime: {
+        [Op.between]: [dtStart, dtEnd]
+      }
     }
   });
+
+  const entries = entriesModel.map(entry => {
+    const dbDateTime = entry.get('dateTime');
+    return {
+      dateTime: dbDateTime.length > 20 ? createTimeStr(dbDateTime) : dbDateTime,
+      event: entry.get('Event').get('Name'),
+      id: entry.get('User').get('UserName'),
+      location: entry.get('Location').get('IpAddress'),
+      nameStr: entry.get('User').get('NameString')
+    };
+  });
+
+  console.log('entries: ', entries);
+
+  const renderInfo = {
+    title: 'Filtered Records',
+    userId,
+    querierId,
+    listAll,
+    dateTime: {
+      date: {
+        start: conditions.dtStart.slice(0, 10),
+        end: conditions.dtEnd.slice(0, 10),
+      },
+      time: {
+        start: conditions.dtStart.slice(11, 16),
+        end: conditions.dtEnd.slice(11, 16)
+      }
+    },
+    isAdmin: arrAdminUser.includes(querierId),
+    home: req.protocol + '://' + req.headers.host,
+    version: pJson.version
+  };
+
+  if (listAll === 'false') {
+    const taStarts = [];
+    const taEnds = [];
+    if (entries.length > 0) {
+      taStarts.push(entries[0]);
+      for (let i = 1; i < entries.length; i++) {
+        if (entries[i].dateTime.slice(0, 10) !== entries[i - 1].dateTime.slice(0, 10)) {
+          taEnds.push(entries[i - 1]);
+          taStarts.push(entries[i]);
+        }
+      }
+      taEnds.push(entries[entries.length - 1]);
+    }
+    res.render('entries', {
+      ...renderInfo,
+      taStarts,
+      taEnds
+    });
+  } else {
+    res.render('entries', {
+      ...renderInfo,
+      entries
+    });
+  }
 };
+
+function createTimeStr(dbDateTime) {
+  const localDateTime = new Date(dbDateTime + "UTC");
+  const dateTime = [localDateTime.getMonth() + 1, localDateTime.getDate(), localDateTime.getHours(), localDateTime.getMinutes(), localDateTime.getSeconds()];
+  const dateTimeArr = dateTime.map(function(num) {
+    let str = num.toString();
+    return str = str.length === 2 ? str : '0'.concat(str);
+  });
+  return localDateTime.getFullYear() + '-' + dateTimeArr[0] + '-' + dateTimeArr[1] + ' ' + dateTimeArr[2] + ':' + dateTimeArr[3] + ':' + dateTimeArr[4];
+}
 
 exports.syncUserMap = syncUserMap;
