5ae44bdc3249ba1371abfcb14c68b22c57503968
garfield <garfield@mail.startek-eng.com>
2018-07-20 14:42:35 +0800

    Fix the bug about the detect timeout feature for the query page

---
+++ public/javascripts/detectQueryIdle.js
@@ -0,0 +1,21 @@
+var idleTime = 0;
+var threshold = 120;
+
+document.addEventListener('keyup', function(event) {
+  idleTime = 0;
+});
+
+document.addEventListener('click', function(event) {
+  idleTime = 0;
+});
+
+function timerIncrement() {
+  const logoutTimer = setTimeout(timerIncrement, 1000);
+  idleTime = idleTime + 1;
+  if (idleTime > threshold) {
+    clearTimeout(logoutTimer);
+    window.location.href = originURL;
+  }
+}
+
+setTimeout(timerIncrement, 1000);
